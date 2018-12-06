fc4c448331dbaba726d077aa990df9860a5e374b
AD <alexander.dunkel@tu-dresden.de>
2018-12-06 11:21:20 +0100

    Minor load-data-fix

--- lbsntransform/classes/load_data.py
+++ lbsntransform/classes/load_data.py
@@ -89,6 +89,7 @@ class LoadData():
            exit(f'Format {format} not supported.')
         return records
 
+    @staticmethod
     def fetch_json_data_from_file(loc_filelist, start_file_id=0, is_stacked_json=False):
         """Read json entries from file.
 
@@ -113,6 +114,7 @@ class LoadData():
             return records
         return None
 
+    @staticmethod
     def fetch_csv_data_from_file(loc_filelist, start_file_id=0):
         """Read csv entries from file (either *.txt or *.csv).
 
@@ -129,20 +131,6 @@ class LoadData():
         if not records:
             return None
         return records
-
-    def add_bundestag_group_example(twitter_records):
-        """ Example: Manually add Group that applies to all entries.
-            To use, must uncomment fieldMapping/extractUser
-        """
-
-        deutscher_bundestag_group = HF.createNewLBSNRecord_with_id(lbsnUserGroup(), "MdB (Bundestag)", twitter_records.origin)
-        dbg_owner = HF.createNewLBSNRecord_with_id(lbsnUser(), "243586130", twitter_records.origin)
-        dbg_owner.user_name = 'wahl_beobachter'
-        dbg_owner.user_fullname = 'Martin Fuchs'
-        deutscher_bundestag_group.user_owner_pkey.CopyFrom(dbg_owner.pkey)
-        deutscher_bundestag_group.usergroup_description = 'Alle twitternden Abgeordneten aus dem Deutschen Bundestag #bundestag'
-        twitter_records.lbsnRecords.AddRecordToDict(dbg_owner)
-        twitter_records.lbsnRecords.AddRecordToDict(deutscher_bundestag_group)
 
     def initialize_output_connection(config):
         """Establishes connection to output DB (Postgres), if set in config"""
