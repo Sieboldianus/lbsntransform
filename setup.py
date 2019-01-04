be9fee3c34abae1eb6132677579b4adb4e7345df
AD <alexander.dunkel@tu-dresden.de>
2019-01-04 11:03:29 +0100

    fix: pypi upload

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ version = {}
 with open("lbsntransform/version.py") as fp:
     exec(fp.read(), version)
 
+# test
 # setuptools dev
 setup(name="lbsntransform",
       version=version['__version__'],
