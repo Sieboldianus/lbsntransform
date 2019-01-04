148550d6e756544002d4f74f6cf84050a91e6038
AD <alexander.dunkel@tu-dresden.de>
2019-01-04 12:36:12 +0100

    fix: crlf line endings

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ version = {}
 with open("lbsntransform/version.py") as fp:
     exec(fp.read(), version)
 
-# test34
+# test35
 # setuptools dev
 setup(name="lbsntransform",
       version=version['__version__'],
