dbf224e5abb7d1fa1925a17551d00c8d5c450df7
Alexander Dunkel <13646666+Sieboldianus@users.noreply.github.com>
2022-10-02 06:27:48 +0200

    Update setup.py
    Fix: Vulnerability in protobuf dependency

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name="lbsntransform",
       include_package_data=True,
       install_requires=[
           'lbsnstructure>=0.5.1',
-          'protobuf<=3.19.4',
+          'protobuf<=3.19.5',
           'psycopg2',
           'ppygis3',
           'shapely',
