8ba2828b5b2acedadceae9522189d4fcfd98d17b
Alexander Dunkel <alexander.dunkel@tu-dresden.de>
2023-05-05 14:04:40 +0200

    Fix argparse indentation error

--- docs/argparse/argparse_doc.py
+++ docs/argparse/argparse_doc.py
@@ -15,38 +15,40 @@ def extract_argscode():
     # extract source code of parse_args
     parse_args_source = inspect.getsource(BaseConfig.get_arg_parser)
     # remove first three lines
-    parse_args_source = parse_args_source[parse_args_source.index('\n')+1:]
-    parse_args_source = parse_args_source[parse_args_source.index('\n')+1:]
-    parse_args_source = parse_args_source[parse_args_source.index('\n')+1:]
+    for __ in range(0, 4):
+        parse_args_source = parse_args_source[parse_args_source.index("\n") + 1 :]
     # insert at beginning
-    parse_args_source = f'parser = argparse.ArgumentParser(prog="lbsntransform")\n{parse_args_source}'
+    parse_args_source = (
+        f'parser = argparse.ArgumentParser(prog="lbsntransform")\n{parse_args_source}'
+    )
     # unindent all other lines
-    parse_args_source = parse_args_source.lstrip().replace('\n        ', '\n')
+    parse_args_source = parse_args_source.lstrip().replace("\n        ", "\n")
     # replace version string
     parse_args_source = parse_args_source.replace(
-        'lbsntransform {__version__}', f'lbsntransform {__version__}')
+        "lbsntransform {__version__}", f"lbsntransform {__version__}"
+    )
     # replace package name
     parse_args_source = parse_args_source.replace(
-        'usage: argdown', 'usage: lbsntransform')
+        "usage: argdown", "usage: lbsntransform"
+    )
     # write argdown and argparse imports first
-    source_file.write('import argparse\n')
-    source_file.write('import argdown\n')
-    source_file.write('from pathlib import Path\n')
+    source_file.write("import argparse\n")
+    source_file.write("import argdown\n")
+    source_file.write("from pathlib import Path\n")
     # fix argparse name
     parse_args_source = parse_args_source.replace(
-        'ArgumentParser()', 'ArgumentParser(prog="lbsntransform")')
+        "ArgumentParser()", 'ArgumentParser(prog="lbsntransform")'
+    )
     # replace method leftover
     parse_args_source = parse_args_source.replace(
-        'return parser', 'args = parser.parse_args()')
+        "return parser", "args = parser.parse_args()"
+    )
     # replace two empty spaces on str line ends with non-breaking spaces,
     # protecting line breaks during python-markdown conversion
-    parse_args_source = parse_args_source.replace(
-        "'  '\n", "'&nbsp;&nbsp;<br>'\n")
-    parse_args_source = parse_args_source.replace(
-        "  '\n", "&nbsp;&nbsp;<br>'\n")
+    parse_args_source = parse_args_source.replace("'  '\n", "'&nbsp;&nbsp;<br>'\n")
+    parse_args_source = parse_args_source.replace("  '\n", "&nbsp;&nbsp;<br>'\n")
     # protected indent
-    parse_args_source = parse_args_source.replace(
-        "'    ", "'&nbsp;&nbsp;&nbsp;&nbsp;")
+    parse_args_source = parse_args_source.replace("'    ", "'&nbsp;&nbsp;&nbsp;&nbsp;")
     # write prepared source code
     source_file.write(parse_args_source)
     source_file.close()
